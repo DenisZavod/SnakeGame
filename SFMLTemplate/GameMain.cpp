d4b700c23153257b038b8c0e1605e007f5100636
DenZavod <zavdenis98@yandex.ru>
2026-03-06 21:53:01 +0400

    snake_game

---
+++ SFMLTemplate/GameMain.cpp
@@ -0,0 +1,65 @@
+﻿#include <SFML/Graphics.hpp>
+#include <cstdlib>
+#include "Game.h"
+
+
+
+int main()
+{
+	using namespace SnakeGame;
+	// Init random number generator
+	unsigned int seed = (unsigned int)time(nullptr); // Get current time as seed. You can also use any other number to fix randomization
+	srand(seed);
+
+	//Init window
+	sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "Sssssnake game!");
+
+	// We now use too much memory for stack, so we need to allocate it on heap
+	SnakeGame::Game* game = new SnakeGame::Game();
+	InitGame(*game);
+
+	// Init game clock
+	sf::Clock gameClock;
+
+	while (window.isOpen())
+	{
+		float startTime = gameClock.getElapsedTime().asSeconds();
+
+		HandleWindowEvents(*game, window);
+
+		if (!window.isOpen())
+		{
+			break;
+		}
+
+		if (UpdateGame(*game, TIME_PER_FRAME))
+		{
+			// Draw everything here
+			// Clear the window first
+			window.clear();
+			DrawGame(*game, window);
+
+			// End the current frame, display window contents on screen
+			window.display();
+		}
+		else
+		{
+			window.close();
+		}
+
+		float endTime = gameClock.getElapsedTime().asSeconds();
+		float deltaTime = endTime - startTime;
+
+		if (deltaTime < TIME_PER_FRAME)
+		{
+			// Reduce framerate to not spam CPU and GPU
+			sf::sleep(sf::seconds(TIME_PER_FRAME - deltaTime));
+		}
+	}
+
+	ShutdownGame(*game);
+	delete game;
+	game = nullptr;
+
+	return 0;
+}
